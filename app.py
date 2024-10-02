d9d3f8965cfa07bd75660c58ceb0c8a506230205
zjowy <162414479+zjowy@users.noreply.github.com>
2024-10-02 10:09:14 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template
 import numpy as np
 import joblib
+import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler
-
+#pipe= pickle.load(open('/Users/shatha/Prog yogesh/our finalised model.pickle','rb'))
 app = Flask(__name__, static_folder='static')
 
 # Load the trained Random Forest model and scaler
@@ -14,13 +15,13 @@ scaler = joblib.load('scaler.joblib')
 def home():
     prediction = None
     if request.method == 'POST':
-        # Get input features from the form
+        # Get input features fropm the form
         features = [
             float(request.form['Purchases']),
-            float(request.form['sbTotal']),
-            float(request.form['sbSolar']),
-            float(request.form['sbNonSolar']),
-            float(request.form['sbHydro']),
+            float(request.form['bTotal']),
+            float(request.form['bSolar']),
+            float(request.form['bNonSolar']),
+            float(request.form['bHydro']),
             float(request.form['mcvTotal']),
             float(request.form['mcvSolar']),
             float(request.form['mcvNonsolar']),
@@ -30,13 +31,13 @@ def home():
             float(request.form['fsvNonSolar']),
             float(request.form['fsvHydro'])
         ]
-        
+
         # Scale the input features
         features_scaled = scaler.transform([features])
-        
+
         # Make prediction
         prediction = rf_model.predict(features_scaled)[0]
-    
+
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
